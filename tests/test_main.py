2e602dab0c9bf6348249ef77f6eae0af63055353
Delgermurun Purevkhuu <pdelgermurun@gmail.com>
2018-10-24 14:59:37 +0900

    test main:test_execute_arg

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,30 +9,26 @@ test_dir = os.path.abspath(os.path.dirname(__file__))
 project_dir = os.path.dirname(test_dir)
 default_config_file = os.path.join(project_dir, 'tests', 'liteclirc')
 
-CLI_ARGS = ['--liteclirc', default_config_file,
-            '--defaults-file', default_config_file,
-            '_test_db']
+CLI_ARGS = ['--liteclirc', default_config_file, '_test_db']
 
 
 @dbtest
 def test_execute_arg(executor):
-    assert 1 + 1 == 3
+    run(executor, 'create table test (a text)')
+    run(executor, 'insert into test values("abc")')
 
-    # run(executor, 'create table test (a text)')
-    # run(executor, 'insert into test values("abc")')
+    sql = 'select * from test;'
+    runner = CliRunner()
+    result = runner.invoke(cli, args=CLI_ARGS + ['-e', sql])
 
-    # sql = 'select * from test;'
-    # runner = CliRunner()
-    # result = runner.invoke(cli, args=CLI_ARGS + ['-e', sql])
+    assert result.exit_code == 0
+    assert '"abc"' in result.output
 
-    # assert result.exit_code == 0
-    # assert '"abc"' in result.output
+    result = runner.invoke(cli, args=CLI_ARGS + ['--execute', sql])
 
-    # result = runner.invoke(cli, args=CLI_ARGS + ['--execute', sql])
+    assert result.exit_code == 0
+    assert '"abc"' in result.output
 
-    # assert result.exit_code == 0
-    # assert '"abc"' in result.output
+    expected = '"a"\n"abc"\n'
 
-    # expected = '"a"\n"abc"\n'
-
-    # assert expected in result.output
+    assert expected in result.output
