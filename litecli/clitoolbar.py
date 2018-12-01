794a93ab65aff0c0b32aefb7db1d6f49aadf956e
Amjith Ramanujam <amjith.r@gmail.com>
2018-11-30 20:58:04 -0800

    Fix the crash caused by pressing F4.

--- litecli/clitoolbar.py
+++ litecli/clitoolbar.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from prompt_toolkit.key_binding.vi_state import InputMode
 from prompt_toolkit.enums import EditingMode
+from prompt_toolkit.application import get_app
 
 
 def create_toolbar_tokens_func(cli, show_fish_help):
@@ -24,7 +25,7 @@ def create_toolbar_tokens_func(cli, show_fish_help):
             result.append(("class:bottom-toolbar.off", "[F3] Multiline: OFF  "))
         if cli.prompt_app.editing_mode == EditingMode.VI:
             result.append(
-                ("class:botton-toolbar.on", "Vi-mode ({})".format(_get_vi_mode(cli)))
+                ("class:botton-toolbar.on", "Vi-mode ({})".format(_get_vi_mode()))
             )
 
         if show_fish_help():
@@ -40,11 +41,11 @@ def create_toolbar_tokens_func(cli, show_fish_help):
     return get_toolbar_tokens
 
 
-def _get_vi_mode(cli):
+def _get_vi_mode():
     """Get the current vi mode for display."""
     return {
         InputMode.INSERT: "I",
         InputMode.NAVIGATION: "N",
         InputMode.REPLACE: "R",
         InputMode.INSERT_MULTIPLE: "M",
-    }[cli.vi_state.input_mode]
+    }[get_app().vi_state.input_mode]
