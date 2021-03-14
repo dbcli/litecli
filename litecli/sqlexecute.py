5b86afa53160551e536718f75124c6a45cd19757
Amjith Ramanujam <amjith@netflix.com>
2021-03-14 13:10:27 -0700

    Fix the utf-8 decoding error.

--- litecli/sqlexecute.py
+++ litecli/sqlexecute.py
@@ -17,6 +17,13 @@ _logger = logging.getLogger(__name__)
 # })
 
 
+def utf8_resilient_decoder(s):
+    try:
+        return s.decode("utf-8")
+    except UnicodeDecodeError:
+        return s.decode("latin-1")
+
+
 class SQLExecute(object):
 
     databases_query = """
@@ -61,6 +68,7 @@ class SQLExecute(object):
             raise Exception("Path does not exist: {}".format(db_dir_name))
 
         conn = sqlite3.connect(database=db_name, isolation_level=None)
+        conn.text_factory = utf8_resilient_decoder
         if self.conn:
             self.conn.close()
 
